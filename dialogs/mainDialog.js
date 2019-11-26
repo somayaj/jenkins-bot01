286324c59c1a98c14d200314582acab877c2c7b1
gosher <somayajulaas@gmail.com>
2019-11-26 10:00:00 -0600

    removed book a flight

--- dialogs/mainDialog.js
+++ dialogs/mainDialog.js
@@ -79,7 +79,7 @@ class MainDialog extends ComponentDialog {
         // Call LUIS and gather any potential booking details. (Note the TurnContext has the response to the prompt)
         const luisResult = await this.luisRecognizer.executeLuisQuery(stepContext.context);
         switch (LuisRecognizer.topIntent(luisResult)) {
-        case 'BookFlight': {
+        /**case 'BookFlight': {
             // Extract the values for the composite entities from the LUIS result.
             const fromEntities = this.luisRecognizer.getFromEntities(luisResult);
             const toEntities = this.luisRecognizer.getToEntities(luisResult);
@@ -96,7 +96,7 @@ class MainDialog extends ComponentDialog {
             // Run the BookingDialog passing in whatever details we have from the LUIS call, it will fill out the remainder.
             return await stepContext.beginDialog('bookingDialog', bookingDetails);
         }
-
+**/
         case 'GetWeather': {
             // We haven't implemented the GetWeatherDialog so we just display a TODO message.
             const getWeatherMessageText = 'TODO: get weather flow here';
